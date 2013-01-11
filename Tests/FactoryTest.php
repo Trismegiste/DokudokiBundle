739cf3314d18adfd4d76dd128647502f6ced7f37
Florent <florent.g@allopneus.com>
2013-01-11 09:59:33 +0100

    ehancing unit test

--- Tests/FactoryTest.php
+++ Tests/FactoryTest.php
@@ -16,16 +16,66 @@ use Trismegiste\DokudokiBundle\Factory;
 class FactoryTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testExperim()
-    {
-        $service = new Factory();
+    protected $service;
 
+    protected function setUp()
+    {
+        $this->service = new Factory();
+    }
+
+    protected function tearDown()
+    {
+        unset($this->service);
+    }
+
+    public function getSampleTree()
+    {
         $obj = new Cart("86 fdfg de fdf");
+        $obj->info = 'nothing to say';
         $obj->addItem(3, new Product('EF85L', 1999));
         $obj->addItem(1, new Product('Bike', 650));
 
-        $dump = $service->desegregation($obj);
-        $restore = $service->create($dump);
+        $dump = array(
+            '_class' => 'Trismegiste\DokudokiBundle\Tests\Cart',
+            'address' => '86 fdfg de fdf',
+            'info' => 'nothing to say',
+            'row' => array(
+                0 => array(
+                    'qt' => 3,
+                    'item' => array(
+                        '_class' => 'Trismegiste\DokudokiBundle\Tests\Product',
+                        'title' => 'EF85L',
+                        'price' => 1999
+                    )
+                ),
+                1 => array(
+                    'qt' => 1,
+                    'item' => array(
+                        '_class' => 'Trismegiste\DokudokiBundle\Tests\Product',
+                        'title' => 'Bike',
+                        'price' => 650,
+                    )
+                )
+            )
+        );
+        return array(array($obj, $dump));
+    }
+
+    /**
+     * @dataProvider getSampleTree
+     */
+    public function testDesegregate($obj, $dumpEqv)
+    {
+        $dump = $this->service->desegregation($obj);
+        $this->assertEquals($dumpEqv, $dump);
+    }
+
+    /**
+     * @dataProvider getSampleTree
+     */
+    public function testRestoring($obj, $dumpEqv)
+    {
+        $restore = $this->service->create($dumpEqv);
         $this->assertEquals($obj, $restore);
     }
 
@@ -34,6 +84,7 @@ class FactoryTest extends \PHPUnit_Framework_TestCase
 class Cart
 {
 
+    public $info = '';
     protected $address;
     private $row = array();
 
