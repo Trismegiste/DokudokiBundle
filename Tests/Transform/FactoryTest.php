1bffb319b038f40d4356d09ba9459e6afb3b4d80
Trismegiste <florent.g@allopneus.com>
2013-01-20 22:49:51 +0100

    splitting test model for unit test

--- Tests/Transform/FactoryTest.php
+++ Tests/Transform/FactoryTest.php
@@ -11,6 +11,8 @@ use Trismegiste\DokudokiBundle\Transform\Mediator\Mediator;
 use Trismegiste\DokudokiBundle\Transform\Skippable;
 use Trismegiste\DokudokiBundle\Transform\Cleanable;
 
+require_once __DIR__ . '/ModelSample.php';
+
 /**
  * FactoryTest test for Factory
  *
@@ -160,92 +162,6 @@ class FactoryTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(42, $dump['answer']);
         $restore = $this->service->create($dump);
         $this->assertEquals(range(1, 100), $restore->getTransient());
-    }
-
-}
-
-class Cart
-{
-
-    public $info = '';
-    protected $address;
-    private $row = array();
-    protected $notInitialized;
-    static public $transientProp = 123;
-
-    public function __construct($addr)
-    {
-        $this->address = $addr;
-    }
-
-    public function addItem($qt, Product $pro)
-    {
-        $this->row[] = array('qt' => $qt, 'item' => $pro);
-    }
-
-}
-
-class Product
-{
-
-    private $title;
-    protected $price = 0;
-
-    public function __construct($tit, $pri)
-    {
-        $this->title = $tit;
-        $this->price = $pri;
-    }
-
-}
-
-class VerifMethod
-{
-
-    protected $vat = 19.6;
-    protected $price;
-
-    public function __construct($x)
-    {
-        $this->price = $x;
-    }
-
-    public function getTotal()
-    {
-        return $this->price * (1 + $this->vat / 100);
-    }
-
-}
-
-class IntoVoid implements Skippable
-{
-    
-}
-
-class Bear implements Cleanable
-{
-
-    protected $answer = 42;
-    protected $transient;
-
-    public function __construct()
-    {
-        $this->transient = range(1, 100);
-    }
-
-    public function getTransient()
-    {
-        return $this->transient;
-    }
-
-    public function wakeup()
-    {
-        $this->__construct();
-    }
-
-    public function sleep()
-    {
-        unset($this->transient);
     }
 
 }
