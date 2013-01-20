241fd5f44e53a490c5cc95a8d8706c163a114439
Trismegiste <florent.g@allopneus.com>
2013-01-20 18:40:49 +0100

    abstracting the mediator to decoupling the regiztering and algorithm

---
+++ Transform/Mediator/AbstractMediator.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * DokudokiBundle
+ */
+
+namespace Trismegiste\DokudokiBundle\Transform\Mediator;
+
+use Trismegiste\DokudokiBundle\Utils\ReflectionClassBC;
+
+/**
+ * Design Pattern : Mediator
+ * Component : Mediator (abstract)
+ * 
+ * Responsible for maintaining a list of Colleague
+ * 
+ * @author flo
+ */
+abstract class AbstractMediator implements RecursiveMapper, TypeRegistry
+{
+
+    protected $mappingColleague = array();
+
+    /**
+     * {@inheritDoc}
+     */
+    public function registerType($name, Mapping $colleague)
+    {
+        if (!is_string($name)) {
+            throw new \InvalidArgumentException('The key type is not a string');
+        }
+
+        $this->mappingColleague[$name] = $colleague;
+    }
+
+}
