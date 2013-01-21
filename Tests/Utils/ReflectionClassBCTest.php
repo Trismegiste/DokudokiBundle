9abab25a431bd3446919cb17ab8610627e64ccb6
Florent <florent.g@allopneus.com>
2013-01-21 10:56:26 +0100

    test for the need of hack with PHP < 5.4

--- Tests/Utils/ReflectionClassBCTest.php
+++ Tests/Utils/ReflectionClassBCTest.php
@@ -6,7 +6,7 @@
 
 namespace Trismegiste\DokudokiBundle\Tests\Utils;
 
-use Trismegiste\DokudokiBundle\Utils\ReflectionClassBC;
+use Trismegiste\DokudokiBundle\Utils\InjectionClass;
 
 /**
  * Description of ReflectionClassBCTest
@@ -16,23 +16,41 @@ use Trismegiste\DokudokiBundle\Utils\ReflectionClassBC;
 class ReflectionClassBCTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testHack()
+    public function getOneDate()
     {
-        $refl = new \Trismegiste\DokudokiBundle\Utils\InjectionClass('DateTime');
+        return array(array(array(
+                    'date' => '2013-01-20 21:55:10',
+                    'timezone_type' => 3,
+                    'timezone' => 'UTC'
+            )));
+    }
+
+    /**
+     * @dataProvider getOneDate
+     * @expectedException PHPUnit_Framework_Error
+     */
+    public function testErrorWhenNoWakeup($param)
+    {
+        $refl = new InjectionClass('DateTime');
         $obj = $refl->newInstanceWithoutConstructor();
-        foreach (array(
-    'date' => '2013-01-20 21:55:10',
-    'timezone_type' => 3,
-    'timezone' => 'UTC'
-        ) as $key => $val) {
+        foreach ($param as $key => $val) {
             $refl->injectProperty($obj, $key, $val);
         }
-        try {
-            $obj->getTimestamp();
-        } catch (Exception $e) {
-            $this->markAsFail();
+        $obj->getTimestamp();
+    }
+
+    /**
+     * @dataProvider getOneDate
+     */
+    public function testHackOk($param)
+    {
+        $refl = new InjectionClass('DateTime');
+        $obj = $refl->newInstanceWithoutConstructor();
+        foreach ($param as $key => $val) {
+            $refl->injectProperty($obj, $key, $val);
         }
         $refl->fixHackBC($obj);
+        $obj->getTimestamp();
         $this->assertEquals(1358718910, $obj->getTimestamp());
     }
 
