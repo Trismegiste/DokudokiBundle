fc0f6d2289959715f29d1f758dfb207693f9e8bd
Florent <florent.g@allopneus.com>
2013-01-25 09:03:17 +0100

    fixing µsec in mongodate

--- Tests/Transform/Mediator/Colleague/DateObjectTest.php
+++ Tests/Transform/Mediator/Colleague/DateObjectTest.php
@@ -30,7 +30,7 @@ class DateObjectTest extends MapperTestTemplate
 
     public function getDataToDb()
     {
-        return array(array(new \DateTime(), new \MongoDate()));
+        return array(array(new \DateTime(), new \MongoDate(time(), 0)));
     }
 
     public function getResponsibleDataToDb()
