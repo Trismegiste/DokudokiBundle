56d30469faf8d08d02c818715e306fbc873689b6
Trismegiste <florent.g@allopneus.com>
2013-02-03 14:16:29 +0100

    stub for builder

---
+++ Transform/Delegation/MappingBuilder.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * DokudokiBundle
+ */
+
+namespace Trismegiste\DokudokiBundle\Transform\Delegation;
+
+use Trismegiste\DokudokiBundle\Transform\Mediator\Mediator;
+use Trismegiste\DokudokiBundle\Transform\Mediator\Colleague;
+
+/**
+ * Design Pattern : Builder
+ * Component : Builder (abstract) 
+ * 
+ * This is a template for a builder 
+ *
+ * @author flo
+ */
+class MappingBuilder
+{
+    //put your code here
+}
