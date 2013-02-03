08fd7ffda95567187e2feba1211b66e9cadaf048
Trismegiste <florent.g@allopneus.com>
2013-02-03 14:33:14 +0100

    add a concrrete builder

--- Transform/Delegation/MappingBuilder.php
+++ Transform/Delegation/MappingBuilder.php
@@ -13,11 +13,37 @@ use Trismegiste\DokudokiBundle\Transform\Mediator\Colleague;
  * Design Pattern : Builder
  * Component : Builder (abstract) 
  * 
- * This is a template for a builder 
+ * This is a template for a builder of delegation of mapping 
+ * @see Transformer 
  *
  * @author flo
  */
 class MappingBuilder
 {
-    //put your code here
+
+    public function createChain()
+    {
+        return new Mediator();
+    }
+
+    public function createNonObject(Mediator $algo)
+    {
+        new Colleague\MapNullable($algo);
+        new Colleague\MapScalar($algo);
+        new Colleague\MapArray($algo);
+    }
+
+    public function createObject(Mediator $algo)
+    {
+        new Colleague\MapObject($algo);
+        new Colleague\MapSkippable($algo);
+        new Colleague\MapMagic($algo);
+    }
+
+    public function createDbSpecific(Mediator $algo)
+    {
+        new Colleague\DateObject($algo);
+        new Colleague\MongoBinData($algo);
+    }
+
 }
