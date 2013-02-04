fbfed98b98b0835c09b9ff34094c1ae50a17d6e7
Trismegiste <florent.g@allopneus.com>
2013-02-04 20:58:33 +0100

    transfering test to mediator

--- Tests/Fixtures/VerifMethod.php
+++ Tests/Fixtures/VerifMethod.php
@@ -23,36 +23,3 @@ class VerifMethod
     }
 
 }
-
-class IntoVoid implements Skippable
-{
-    
-}
-
-class Bear implements Cleanable
-{
-
-    protected $answer = 42;
-    protected $transient;
-
-    public function __construct()
-    {
-        $this->transient = range(1, 100);
-    }
-
-    public function getTransient()
-    {
-        return $this->transient;
-    }
-
-    public function wakeup()
-    {
-        $this->__construct();
-    }
-
-    public function sleep()
-    {
-        unset($this->transient);
-    }
-
-}
--- Tests/Transform/Delegation/Stage/FirstTest.php
+++ Tests/Transform/Delegation/Stage/FirstTest.php
@@ -74,4 +74,82 @@ class FirstTest extends AbstractStageTest
         return new First();
     }
 
+    public function testCallingRestore()
+    {
+        $obj = new Fixtures\VerifMethod(100);
+        $dump = $this->mediator->recursivDesegregate($obj);
+        $restore = $this->mediator->recursivCreate($dump);
+        $this->assertInstanceOf('Trismegiste\DokudokiBundle\Tests\Fixtures\VerifMethod', $restore);
+        $this->assertEquals(119.6, $restore->getTotal());
+    }
+
+    /**
+     * @expectedException \DomainException
+     */
+    public function testRootClassEmpty()
+    {
+        $obj = $this->mediator->recursivCreate(array(MapObject::FQCN_KEY => null, 'answer' => 42));
+    }
+
+    /**
+     * @expectedException \DomainException
+     */
+    public function testLeafClassEmpty()
+    {
+        $obj = $this->mediator->recursivCreate(
+                array(
+                    MapObject::FQCN_KEY => 'stdClass',
+                    'child' => array(MapObject::FQCN_KEY => null, 'answer' => 42)
+                )
+        );
+    }
+
+    /**
+     * @expectedException \DomainException
+     */
+    public function testRootClassNotFound()
+    {
+        $this->mediator->recursivCreate(array(MapObject::FQCN_KEY => 'Snark', 'answer' => 42));
+    }
+
+    /**
+     * @expectedException \DomainException
+     */
+    public function testLeafClassNotFound()
+    {
+        $obj = $this->mediator->recursivCreate(
+                array(
+                    MapObject::FQCN_KEY => 'stdClass',
+                    'child' => array(MapObject::FQCN_KEY => 'Snark', 'answer' => 42)
+                )
+        );
+    }
+
+    public function testSkippable()
+    {
+        $obj = new Fixtures\IntoVoid();
+        $dump = $this->mediator->recursivDesegregate($obj);
+        $this->assertNull($dump);
+    }
+
+    public function testChildSkippable()
+    {
+        $obj = new \stdClass();
+        $obj->dummy = new Fixtures\IntoVoid();
+        $obj->product = new Fixtures\Product("aaa", 23);
+        $dump = $this->mediator->recursivDesegregate($obj);
+        $this->assertNull($dump['dummy']);
+        $this->assertNotNull($dump['product']);
+    }
+
+    public function testCleanable()
+    {
+        $obj = new Fixtures\Bear();
+        $dump = $this->mediator->recursivDesegregate($obj);
+        $this->assertNull($dump['transient']);
+        $this->assertEquals(42, $dump['answer']);
+        $restore = $this->mediator->recursivCreate($dump);
+        $this->assertEquals(range(1, 100), $restore->getTransient());
+    }
+
 }
