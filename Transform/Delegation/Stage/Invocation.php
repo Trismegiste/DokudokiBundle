eaa4bc65e01889c992279bcf960b346a7fb83b7c
Trismegiste <florent.g@allopneus.com>
2013-02-05 21:40:50 +0100

    white magic builder with alias

--- Transform/Delegation/Stage/Invocation.php
+++ Transform/Delegation/Stage/Invocation.php
@@ -17,8 +17,8 @@ use Trismegiste\DokudokiBundle\Transform\Mediator\TypeRegistry;
  * for class keys. Zero configuration needed.
  * Only magic for storing the type of object.
  * Warning : Can be very messy in queries
- * Usefull : when you have a model and don't want to alais each class. Also
- * usefull for others app, you don't need to repeat the alias map, only the
+ * Usefull : when you have a model and don't want to alias each class. Also
+ * usefull for other apps, you don't need to repeat the alias map, only the
  * model classes are needed.
  * Fail when a class not exists.
  *
---
+++ Transform/Delegation/Stage/WhiteMagic.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * DokudokiBundle
+ */
+
+namespace Trismegiste\DokudokiBundle\Transform\Delegation\Stage;
+
+use Trismegiste\DokudokiBundle\Transform\Mediator\Colleague;
+use Trismegiste\DokudokiBundle\Transform\Mediator\TypeRegistry;
+
+/**
+ * Design Pattern : Builder
+ * Component : Builder (concrete) 
+ * 
+ * A builder to automagically store object in database by using an alias map 
+ * for FQCN. You need to configure each class or it must implement Skippable
+ *
+ * Very strict : if a class does not exists in the alias map, it turns into
+ * an array.
+ *
+ */
+class WhiteMagic extends AbstractStage
+{
+
+    protected $aliasMap;
+
+    public function __construct(array $map)
+    {
+        $this->aliasMap = $map;
+    }
+
+    public function createObject(TypeRegistry $algo)
+    {
+        new Colleague\MapSkippable($algo);
+        new Colleague\MapAlias($algo, $this->aliasMap);
+    }
+
+}
