47e685802467848dc45e012f2c2b510484cf460f
Florent <florent.g@allopneus.com>
2013-02-08 10:23:42 +0100

    bootstrap ok

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,12 +1,12 @@
 <?php
 
-define('FIXTURES_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'fixtures' . DIRECTORY_SEPARATOR);
+/*
+ * bootstrapping the test suite with composer
+ */
 
-// see assetic for the bootstrap with composer and travis
+if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
+    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL);
+}
 
-spl_autoload_register(function ($class) {
-            if (preg_match('#^Trismegiste\\\\DokudokiBundle\\\\(.+)$#', $class, $ret)) {
-                $relPath = str_replace('\\', DIRECTORY_SEPARATOR, $ret[1]);
-                require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . $relPath . '.php';
-            }
-        });
