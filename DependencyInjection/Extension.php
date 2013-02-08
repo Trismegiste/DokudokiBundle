074c599899fec70c18a01b0583936b68e2ced964
Florent <florent.g@allopneus.com>
2013-02-08 14:45:14 +0100

    injecting parameters config in services.xml

--- DependencyInjection/Extension.php
+++ DependencyInjection/Extension.php
@@ -4,8 +4,8 @@ namespace Trismegiste\DokudokiBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension as BaseExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
  * Extension for the configuration of this bundle
@@ -18,16 +18,23 @@ class Extension extends BaseExtension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        // connector
-        $container->setDefinition('dokudoki.connector', new Definition(
-                        'Trismegiste\DokudokiBundle\Persistence\Connector',
-                        array($config)
-                )
-        );
-        // collection
-        $container->setDefinition('dokudoki.collection', new Definition('MongoCollection'))
-                ->setFactoryService('dokudoki.connector')
-                ->setFactoryMethod('getCollection');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
+
+        $container->setParameter('dokudoki.config.server', $config['server']);
+        $container->setParameter('dokudoki.config.database', $config['database']);
+        $container->setParameter('dokudoki.config.collection', $config['collection']);
+//        // connector
+//        $container->setDefinition('dokudoki.connector', new Definition(
+//                        'Trismegiste\DokudokiBundle\Persistence\Connector',
+//                        array($config)
+//                )
+//        );
+//
+//        // collection
+//        $container->setDefinition('dokudoki.collection', new Definition('MongoCollection'))
+//                ->setFactoryService('dokudoki.connector')
+//                ->setFactoryMethod('getCollection');
 //        // factory
 //        $container->setDefinition('dokudoki.factory', new Definition(
 //                        'Trismegiste\DokudokiBundle\Model\Factory',
