89321e3b1668b139a63681d5e2b80df24f006731
Florent <florent.g@allopneus.com>
2013-02-19 11:27:44 +0100

    adding test for readme

--- Tests/ReadmeExampleTest.php
+++ Tests/ReadmeExampleTest.php
@@ -4,70 +4,137 @@
  * DokudokiBundle
  */
 
-namespace Trismegiste\DokudokiBundle\Tests\Form;
+namespace Trismegiste\DokudokiBundle\Tests\Form {
 
-use Symfony\Component\Form\Forms;
-use Trismegiste\DokudokiBundle\Form\MagicFormType;
+    use Symfony\Component\Form\Forms,
+        Trismegiste\DokudokiBundle\Form\MagicFormType,
+        Trismegiste\DokudokiBundle\Transform\Delegation\Stage;
 
-/**
- * ReadmeExample is a ...
- *
- * @author florent
- */
-class ReadmeExampleTest extends FunctionalTestForm
-{
-
-    protected $formFactory;
-    protected $repository;
-    protected $collection;
-
-    protected function setUp()
+    /**
+     * ReadmeExample is a ...
+     *
+     * @author florent
+     */
+    class ReadmeExampleTest extends FunctionalTestForm
     {
-        // Set up the Form component
-        $this->formFactory = Forms::createFormFactoryBuilder()
-                ->addType(new MagicFormType())
-                ->getFormFactory();
 
-        $connector = new \Trismegiste\DokudokiBundle\Tests\Persistence\ConnectorTest();
-        $this->collection = $connector->testCollection();
-        $facade = new \Trismegiste\DokudokiBundle\Facade\Provider($this->collection);
-        $this->repository = $facade->createRepository(new \Trismegiste\DokudokiBundle\Transform\Delegation\Stage\BlackMagic());
+        protected $formFactory;
+        protected $blackmagic;
+        protected $whitemagic;
+        protected $invocation;
+        protected $collection;
+
+        protected function setUp()
+        {
+            // Set up the Form component
+            $this->formFactory = Forms::createFormFactoryBuilder()
+                    ->addType(new MagicFormType())
+                    ->getFormFactory();
+
+            $connector = new \Trismegiste\DokudokiBundle\Tests\Persistence\ConnectorTest();
+            $this->collection = $connector->testCollection();
+            $facade = new \Trismegiste\DokudokiBundle\Facade\Provider($this->collection);
+            $this->blackmagic = $facade->createRepository(new Stage\BlackMagic());
+            $this->invocation = $facade->createRepository(new Stage\Invocation());
+            $this->whitemagic = $facade->createRepository(new Stage\WhiteMagic(array('product' => 'Some\Sample\Product')));
+        }
+
+        protected function tearDown()
+        {
+            unset($this->formFactory);
+        }
+
+        public function testBlackMagicExample()
+        {
+            // construct a form
+            $form = $this->formFactory
+                    ->createBuilder('magic_form', null, array('class_key' => 'product'))
+                    ->add('title')
+                    ->add('price')
+                    ->getForm();
+            // bind data to th form
+            $form->bind(array('title' => 'EF-85 L', 'price' => 2000));
+            $doc = $form->getData();
+            // getting the magic document
+            $this->assertInstanceOf('Trismegiste\DokudokiBundle\Magic\Document', $doc);
+            $this->assertEquals('product', $doc->getClassName());
+            $this->assertEquals('EF-85 L', $doc->getTitle());
+            // persistence with blackmagic repository
+            $this->blackmagic->persist($doc);
+            // retrieving the content in the MongoDB
+            $dump = $this->collection->findOne(array('_id' => $doc->getId()));
+            $this->assertEquals('product', $dump['-class']);
+            $this->assertEquals('EF-85 L', $dump['title']);
+            // restoring with blackmagic repository
+            $restore = $this->blackmagic->findByPk((string) $doc->getId());
+            $this->assertInstanceOf('Trismegiste\DokudokiBundle\Magic\Document', $restore);
+            $this->assertEquals('product', $restore->getClassName());
+            $this->assertEquals('EF-85 L', $restore->getTitle());
+        }
+
+        public function testInvocationExample()
+        {
+            // simple object
+            $doc = new \Some\Sample\Product('EF-85 L', 2000);
+            // persisting
+            $this->invocation->persist($doc);
+            // restoring with invocation repository
+            $restore = $this->invocation->findByPk((string) $doc->getId());
+            $this->assertInstanceOf('Some\Sample\Product', $restore);
+            // retrieving the content in the MongoDB
+            $dump = $this->collection->findOne(array('_id' => $doc->getId()));
+            $this->assertEquals('Some\Sample\Product', $dump['-fqcn']);
+            $this->assertEquals('EF-85 L', $dump['title']);
+            $this->assertEquals(2000, $dump['price']);
+        }
+
+        public function testWhiteMagicExample()
+        {
+            // simple object
+            $doc = new \Some\Sample\Product('EF-85 L', 2000);
+            // persisting
+            $this->whitemagic->persist($doc);
+            // restoring with whitemagic repository
+            $restore = $this->whitemagic->findByPk((string) $doc->getId());
+            $this->assertInstanceOf('Some\Sample\Product', $restore);
+            // retrieving the content in the MongoDB
+            $dump = $this->collection->findOne(array('_id' => $doc->getId()));
+            $this->assertEquals('product', $dump['-class']);
+            $this->assertEquals('EF-85 L', $dump['title']);
+            $this->assertEquals(2000, $dump['price']);
+        }
+
     }
 
-    protected function tearDown()
-    {
-        unset($this->formFactory);
-    }
+}
 
-    public function testBlackMagicExample()
+namespace Some\Sample {
+
+    use Trismegiste\DokudokiBundle\Persistence\Persistable;
+
+    class Product implements Persistable
     {
-        // construct a form
-        $form = $this->formFactory
-                ->createBuilder('magic_form', null, array('class_key' => 'product'))
-                ->add('title')
-                ->add('price')
-                ->getForm();
-        // bind data to th form
-        $form->bind(array('title' => 'EF-85 L', 'price' => 2000));
-        $doc = $form->getData();
-        // getting the magic document
-        $this->assertInstanceOf('Trismegiste\DokudokiBundle\Magic\Document', $doc);
-        $this->assertEquals('product', $doc->getClassName());
-        $this->assertEquals('EF-85 L', $doc->getTitle());
-        $this->assertEquals(2000, $doc->getPrice());
-        // persistence with repository
-        $this->repository->persist($doc);
-        // retrieving the content in the MongoDB
-        $dump = $this->collection->findOne(array('_id' => $doc->getId()));
-        $this->assertEquals('product', $dump['-class']);
-        $this->assertEquals('EF-85 L', $dump['title']);
-        $this->assertEquals(2000, $dump['price']);
-        // restoring with repository
-        $restore = $this->repository->findByPk((string) $doc->getId());
-        $this->assertInstanceOf('Trismegiste\DokudokiBundle\Magic\Document', $restore);
-        $this->assertEquals('product', $restore->getClassName());
-        $this->assertEquals('EF-85 L', $restore->getTitle());
-        $this->assertEquals(2000, $restore->getPrice());
+
+        protected $id;
+        protected $title;
+        protected $price;
+
+        public function __construct($title, $price)
+        {
+            $this->title = $title;
+            $this->price = $price;
+        }
+
+        public function getId()
+        {
+            return $this->id;
+        }
+
+        public function setId(\MongoId $pk)
+        {
+            $this->id = $pk;
+        }
+
     }
 
 }
